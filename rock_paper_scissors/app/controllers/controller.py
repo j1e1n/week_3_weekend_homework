0fb660606ee1562b4f53f91e1ee8a8dd97b58a17
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-01-23 14:40:04 +0000

    first draft

---
+++ rock_paper_scissors/app/controllers/controller.py
@@ -0,0 +1,44 @@
+from app import app
+from flask import render_template, request, redirect
+from app.models.game import *
+from app.models.player import Player
+
+
+@app.route('/')
+def index():
+    return render_template("index.html", title="Home")
+
+@app.route('/play')
+def play_page():
+    return render_template("play.html", title="Play")
+
+@app.route('/winner', methods=['POST'])
+def submit_choices():
+    p1_name = request.form['player_1']
+    p1_choice = request.form['choice_1']
+    p2_name = request.form['player_2']
+    p2_choice = request.form['choice_2']
+    p1 = Player(p1_name, p1_choice)
+    p2 = Player(p2_name, p2_choice)
+    winner = Game.get_winner(p1, p2)
+    return render_template("winner.html", title="winner", winner=winner)
+
+
+
+
+    # choice_1 = request.form['choice_1']
+    # choice_2 = request.form['choice_2']
+    # if choice_1 == "rock":
+    #     if choice_2 == "scissors":
+    #         return redirect('/rock/scissors')
+    #     return redirect('/rock/paper')
+
+
+
+    
+        
+
+
+    
+
+
---
+++ rock_paper_scissors/app/models/game.py
@@ -0,0 +1,28 @@
+class Game:
+
+    
+    def get_winner(p1, p2):
+        if p1.choice == "rock":
+            if p2.choice == "scissors":
+                return p1
+            else:
+                return p2
+
+        elif p1.choice == "paper":
+            if p2.choice == "rock":
+                return p1
+            else:
+                return p2
+
+        elif p1.choice == "scissors":
+            if p2.choice == "paper":
+                return p1
+            else:
+                return p2
+
+
+
+      
+    
+
+
