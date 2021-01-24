4c72711f2ef9a39185d651c2953810953444d659
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-01-24 19:20:39 +0000

    second attempt

--- rock_paper_scissors/app/controllers/controller.py
+++ rock_paper_scissors/app/controllers/controller.py
@@ -8,30 +8,31 @@ from app.models.player import Player
 def index():
     return render_template("index.html", title="Home")
 
-@app.route('/play')
-def play_page():
-    return render_template("play.html", title="Play")
+# @app.route('/play')
+# def play_page():
+#     return render_template("play.html", title="Play")
 
-@app.route('/winner', methods=['POST'])
-def submit_choices():
-    p1_name = request.form['player_1']
-    p1_choice = request.form['choice_1']
-    p2_name = request.form['player_2']
-    p2_choice = request.form['choice_2']
+# @app.route('/result', methods=['POST'])
+# def submit_choices():
+#     p1_name = request.form['player_1']
+#     p1_choice = request.form['choice_1']
+#     p2_name = request.form['player_2']
+#     p2_choice = request.form['choice_2']
+#     p1 = Player(p1_name, p1_choice)
+#     p2 = Player(p2_name, p2_choice)
+#     winner = Game.get_winner(p1, p2)
+#     return render_template("winner.html", title="Winner", winner=winner, p1_choice=p1_choice, p2_choice=p2_choice)
+
+@app.route('/<p1_choice>/<p2_choice>', methods=['POST', 'GET'])
+def play_via_url(p1_choice, p2_choice):
+    p1_name = 'Player 1'
+    p2_name = 'Player 2'
     p1 = Player(p1_name, p1_choice)
     p2 = Player(p2_name, p2_choice)
     winner = Game.get_winner(p1, p2)
-    return render_template("winner.html", title="Winner", winner=winner)
+    return render_template("winner.html", title="Winner", winner=winner, p1_choice=p1_choice, p2_choice=p2_choice)      
 
 
-
-
-    # choice_1 = request.form['choice_1']
-    # choice_2 = request.form['choice_2']
-    # if choice_1 == "rock":
-    #     if choice_2 == "scissors":
-    #         return redirect('/rock/scissors')
-    #     return redirect('/rock/paper')
 
 
 
--- rock_paper_scissors/app/models/game.py
+++ rock_paper_scissors/app/models/game.py
@@ -24,7 +24,7 @@ class Game:
                 return p2
 
 
-
+  
       
     
 
