b2e899688bb8f5ef086fc7c9ad56c9d5b79090bd
Jennifer W <jenniferw@Jennifers-MacBook-Pro.local>
2021-01-25 00:38:34 +0000

    added css and player can add name

--- rock_paper_scissors/app/controllers/controller.py
+++ rock_paper_scissors/app/controllers/controller.py
@@ -8,20 +8,20 @@ from app.models.player import Player
 def index():
     return render_template("index.html", title="Home")
 
-# @app.route('/play')
-# def play_page():
-#     return render_template("play.html", title="Play")
+@app.route('/play')
+def play_page():
+    return render_template("play.html", title="Play")
 
-# @app.route('/result', methods=['POST'])
-# def submit_choices():
-#     p1_name = request.form['player_1']
-#     p1_choice = request.form['choice_1']
-#     p2_name = request.form['player_2']
-#     p2_choice = request.form['choice_2']
-#     p1 = Player(p1_name, p1_choice)
-#     p2 = Player(p2_name, p2_choice)
-#     winner = Game.get_winner(p1, p2)
-#     return render_template("winner.html", title="Winner", winner=winner, p1_choice=p1_choice, p2_choice=p2_choice)
+@app.route('/result', methods=['POST'])
+def submit_choices():
+    p1_name = request.form['player_1']
+    p1_choice = request.form['choice_1']
+    p2_name = request.form['player_2']
+    p2_choice = request.form['choice_2']
+    p1 = Player(p1_name, p1_choice)
+    p2 = Player(p2_name, p2_choice)
+    winner = Game.get_winner(p1, p2)
+    return render_template("winner.html", title="Winner", winner=winner, p1_choice=p1_choice, p2_choice=p2_choice)
 
 @app.route('/<p1_choice>/<p2_choice>', methods=['POST', 'GET'])
 def play_via_url(p1_choice, p2_choice):
